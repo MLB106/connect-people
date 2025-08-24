6c96a101048c794ee5acb3d8148d7178349d6136
MLB <connect_project_dz@yahoo.com>
2025-08-24 18:19:17 +0200

    fix: jest config + premier test

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+﻿export default {
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  testEnvironment: 'node',
+  transform: { '^.+\\.ts$': ['ts-jest', { useESM: true }] },
+  moduleNameMapper: { '^(\\.{1,2}/.*)\\.js$': '' },
+  testMatch: ['**/__tests__/**/*.test.ts'],
+};
--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
 
 i18n.configure({
-  locales: ['fr', 'en', 'de', 'es', 'it', 'pt'],
+  locales: ['fr', 'en', 'de', 'es', 'it', 'pt', 'ar'],
   defaultLocale: 'fr',
   directory: path.join(__dirname, '..', 'locales'),
   objectNotation: true,
